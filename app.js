9fa89c92f2d21a84bffc555c38f9107461c9c709
Akash Gajjar <akashgajjar8@gmail.com>
2018-06-28 21:43:38 +0530

    modified server and client to clear log every 10 minutes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ wss.on('connection', function(ws) {
   ws.send("<span class='serv-label'>Server> </span>please use 'username:&lt;nick&gt;' to set a nickname, otherwise your default nick will be anon(id)");
   ws.send("<span class='serv-label'>Server> </span>use Shift+Enter for multiline message.");
   ws.send("<span class='serv-label'>Server> </span>use 'exit' to disconnect from the server.");
+  ws.send("<span class='serv-label'>Server> </span>The chat log clears every 10 minutes.");
   ws.id = 'anon' + cid;
   cid++;
   messages.forEach(function(msg) {
@@ -31,11 +32,22 @@ wss.on('connection', function(ws) {
       ws.id = message.split(':')[1];
       messages.push(`Client ${prev} changed nickname to ${ws.id}`);
       sendToAll(`Client ${prev} changed nickname to ${ws.id}`);
-    } else {
+    }else {
       messages.push(`<span class="label">${ws.id}> </span><pre class="usr-msg">${message}</pre>`);
       sendToAll(`<span class="label">${ws.id}> </span><pre class="usr-msg">${message}</pre>`);
     }
   });
+  setInterval(()=>{
+    messages=[];
+    wss.clients.forEach(function(client) {
+      client.send('clear');
+      ws.send('<span class="serv-label">Server> </span>W3lc0m3 70 cyb3r cha7.');
+      ws.send("<span class='serv-label'>Server> </span>please use 'username:&lt;nick&gt;' to set a nickname, otherwise your default nick will be anon(id)");
+      ws.send("<span class='serv-label'>Server> </span>use Shift+Enter for multiline message.");
+      ws.send("<span class='serv-label'>Server> </span>use 'exit' to disconnect from the server.");
+      ws.send("<span class='serv-label'>Server> </span>The chat log clears every 10 minutes.");
+    });
+  },600000);
 });
 
 function sendToAll(message) {
